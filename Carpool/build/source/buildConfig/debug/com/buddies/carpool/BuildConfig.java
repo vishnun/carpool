3d9da655454cee79574e61efcf232184ed2354ad
vishnun <vishnun@thoughtworks.com>
2013-07-02 22:47:35 +0530

    First Commit - Initial project setup with basic example.

---
+++ Carpool/build/source/buildConfig/debug/com/buddies/carpool/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.buddies.carpool;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
---
+++ Carpool/src/main/java/com/buddies/carpool/HomeScreenActivity.java
@@ -0,0 +1,47 @@
+package com.buddies.carpool;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class HomeScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        Button submitBtn = (Button) findViewById(R.id.greet_here);
+        Button newActivityBtn = (Button)findViewById(R.id.green_in_new_activity);
+
+        newActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = String.valueOf(((TextView) findViewById(R.id.editText)).getText());
+                Intent intent = new Intent(getApplicationContext(), GreetInNewActivity.class);
+                intent.putExtra("name",name);
+                startActivity(intent);
+            }
+        });
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = String.valueOf(((TextView) findViewById(R.id.editText)).getText());
+                ((TextView)findViewById(R.id.textView)).setText("Hello " + text);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_screen, menu);
+        return true;
+    }
+    
+}
