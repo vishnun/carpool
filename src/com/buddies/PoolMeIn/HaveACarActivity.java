9b9869bdd75b9d3fab84ed3f85dc9b5fe994516a
vishnun <vishnun@thoughtworks.com>
2013-07-14 15:44:59 +0530

    Moving to Idea IntelliJ from Android Studio temporarily.

---
+++ src/com/buddies/PoolMeIn/HaveACarActivity.java
@@ -0,0 +1,12 @@
+package com.buddies.carpool;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HaveACarActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.have_car_form);
+    }
+}
