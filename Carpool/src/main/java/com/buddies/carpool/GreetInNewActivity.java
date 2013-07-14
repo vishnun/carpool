7c92775785c55a0419b15b5364ba56179f006439
vishnun <vishnun@thoughtworks.com>
2013-07-14 14:54:40 +0530

    Updating the gitIgnore file and adding the src files back to git.

---
+++ Carpool/src/main/java/com/buddies/carpool/GreetInNewActivity.java
@@ -0,0 +1,18 @@
+package com.buddies.carpool;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class GreetInNewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.greet_in_new_screen);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        ((TextView) findViewById(R.id.greetText)).setText("Hello " + name);
+    }
+}
---
+++ Carpool/src/main/java/com/buddies/carpool/NeedACarActivity.java
@@ -0,0 +1,12 @@
+package com.buddies.carpool;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NeedACarActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.need_a_car_form);
+    }
+}
